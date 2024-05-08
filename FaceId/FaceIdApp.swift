5187d7fcd0df9b2a8059293c3ec6863dcb652e98
zhoujianbang <zhoujianbang@meicai.cn>
2024-05-08 11:28:07 +0800

    Initial Commit

---
+++ FaceId/FaceIdApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceIdApp.swift
+//  FaceId
+//
+//  Created by zhou jianbang on 2024/5/8.
+//
+
+import SwiftUI
+
+@main
+struct FaceIdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
